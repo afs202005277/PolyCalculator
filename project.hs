e193bb0bf8fcf31f52441988ba4ead58cddf36ed
Andre Sousa <up202005277@up.pt>
2022-10-06 17:51:18 +0100

    Parsing with signs

--- project.hs
+++ project.hs
@@ -2,21 +2,24 @@ import Data.Char
 import Data.List
 import Data.Maybe (fromJust, isJust)
 
-data Termo = Termo Coef Variable Exp
+data Termo = Termo Sign Coef Variable Exp deriving (Eq, Show)
 
-type Coef = Float
+type Sign = Char
+
+type Coef = Int
 
 type Variable = String
 
 type Exp = Int
 
 convertToExp :: String -> Int
-convertToExp str
-  | all isDigit str = read str :: Int
+convertToExp [] = 1
+convertToExp (x:xs)
+  | all isDigit (x:xs) = read (x:xs) :: Int
   | otherwise = 1
 
 wordSplit :: String -> [Termo]
-wordSplit str = [Termo (read (takeWhile isDigit uterm) :: Float) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | uterm <- words str, let idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0, length uterm > 1]
+wordSplit str = [Termo signal (convertToExp ((takeWhile isDigit uterm))) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | idx_term <- [0..length (words str)-1], let uterm = words str !! idx_term; idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0; signal = if idx_term > 0 && head (words str !! (idx_term-1)) == '-' then '-' else '+', uterm /= "+" && uterm /= "-"]
 
 {-
 stringToInternal :: [Termo] -> IO ()
