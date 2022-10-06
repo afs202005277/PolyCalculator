b631bf8acf499948a3460fd9e879df4540a24e54
Andre Sousa <up202005277@up.pt>
2022-10-06 17:58:16 +0100

    Perfected signs

--- project.hs
+++ project.hs
@@ -2,9 +2,9 @@ import Data.Char
 import Data.List
 import Data.Maybe (fromJust, isJust)
 
-data Termo = Termo Sign Coef Variable Exp deriving (Eq, Show)
+data Termo = Termo Coef Variable Exp deriving (Eq, Show)
 
-type Sign = Char
+-- type Sign = Char
 
 type Coef = Int
 
@@ -15,11 +15,14 @@ type Exp = Int
 convertToExp :: String -> Int
 convertToExp [] = 1
 convertToExp (x:xs)
+  | x == '+' && not (null xs) = read xs :: Int
+  | x == '-' && not (null xs) = read (x:xs) :: Int
+  | x == '-' && null xs = -1
   | all isDigit (x:xs) = read (x:xs) :: Int
   | otherwise = 1
 
 wordSplit :: String -> [Termo]
-wordSplit str = [Termo signal (convertToExp ((takeWhile isDigit uterm))) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | idx_term <- [0..length (words str)-1], let uterm = words str !! idx_term; idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0; signal = if idx_term > 0 && head (words str !! (idx_term-1)) == '-' then '-' else '+', uterm /= "+" && uterm /= "-"]
+wordSplit str = [Termo (convertToExp (signal : takeWhile isDigit uterm)) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | idx_term <- [0..length (words str)-1], let uterm = words str !! idx_term; idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0; signal = if idx_term > 0 && head (words str !! (idx_term-1)) == '-' then '-' else '+', uterm /= "+" && uterm /= "-"]
 
 {-
 stringToInternal :: [Termo] -> IO ()
