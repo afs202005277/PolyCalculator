3881798b23b2be054a647579fb2aa6658d22469d
Andre Sousa <up202005277@up.pt>
2022-10-06 15:59:26 +0100

    Using Termo data structure

--- project.hs
+++ project.hs
@@ -5,16 +5,18 @@ import Data.Maybe (fromJust, isJust)
 data Termo = Termo Coef Variable Exp
 
 type Coef = Float
+
 type Variable = String
-type Exp = Float
+
+type Exp = Int
 
 convertToExp :: String -> Int
 convertToExp str
-            | all isDigit str = read str :: Int
-            | otherwise = 1
+  | all isDigit str = read str :: Int
+  | otherwise = 1
 
-wordSplit :: String -> [[String]]
-wordSplit str = [[takeWhile isDigit uterm, takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm), convertToExp (dropWhile (const False) (drop idx uterm))] | uterm <- words str, let idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust(elemIndex '^' uterm)+1 else 0, length uterm > 1]
+wordSplit :: String -> [Termo]
+wordSplit str = [Termo (read (takeWhile isDigit uterm) :: Float) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | uterm <- words str, let idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0, length uterm > 1]
 
 {-
 stringToInternal :: [Termo] -> IO ()
