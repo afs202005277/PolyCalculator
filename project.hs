c63889cb53afdb7050c51b0f93f08e3b574dec69
Andre Sousa <up202005277@up.pt>
2022-10-07 19:20:06 +0100

    Upgrades

--- project.hs
+++ project.hs
@@ -4,26 +4,20 @@ import Data.Maybe (fromJust, isJust)
 
 type Polynom = [Termo]
 
-data Termo = Termo { coef :: Int, variable :: String, exp :: Int } deriving (Eq, Show)
-
-(!!) :: (Int, String, Int) -> Int -> Either Int String
-(!!) (a:b:c:_) idx
-            | idx == 0 = a
-            | idx == 1 = b
-            | idx == 2 = c
-            | otherwise = error "OUT OF BOUNDS"
+data Termo = Termo { coef :: Int, variable :: String, expo :: Int } deriving (Eq, Show)
 
 sumToIndex :: Int -> Int -> Int -> Polynom -> Polynom
+sumToIndex _ _ _ [] = error "EMPTY POLYNOM"
 sumToIndex element idx currentIdx (p:ps)
-                          | idx == currentIdx = Termo ((extractValues p Main.!! 0) + element) ((extractValues p) Main.!! 1) ((extractValues p) Main.!! 2) : ps
+                          | idx == currentIdx = Termo (coef p + element) (variable p) (expo p) : ps
                           | otherwise = p : sumToIndex element idx (currentIdx+1) ps
 
+findMatchingTerm :: Termo -> Polynom -> Int
+findMatchingTerm _ [] = -1
+findMatchingTerm needle (p:ps)
+                    | coef p == coef needle && variable p == variable needle = 0
+                    | otherwise = 1 + findMatchingTerm needle (p:ps)
 
-extractValues :: Termo -> (Int, String, Int)
-extractValues (Termo a b c) = (a, b, c)
-
-getVariableList :: Polynom -> [String]
-getVariableList p = [extractValues tmp Main.!! 1 | tmp <- p]
 
 convertToExp :: String -> Int
 convertToExp [] = 1
@@ -38,8 +32,12 @@ wordSplit :: String -> Polynom
 wordSplit str = [Termo (convertToExp (signal : takeWhile isDigit uterm)) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | idx_term <- [0 .. length (words str) -1], let uterm = words str Prelude.!! idx_term; idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0; signal = if idx_term > 0 && head (words str Prelude.!! (idx_term -1)) == '-' then '-' else '+', uterm /= "+" && uterm /= "-"]
 
 addTerms :: Polynom -> Polynom
-addTerms (p1:ps)
-            | Data.Maybe.isJust(elemIndex (head lista) (tail lista)) = addTerms sumToIndex(extractValues p1 Main.!! 0, fromJust(elemIndex (head lista) (tail lista)), 0, ps)
+addTerms [] = []
+addTerms (p:ps)
+            | Data.Maybe.isJust possibleIdx= addTerms (sumToIndex(coef p) (fromJust possibleIdx) 0 ps)
+            | otherwise = p : addTerms ps
+            where lista = [variable tmp | tmp <- p:ps]
+                  possibleIdx = elemIndex (head lista) (tail lista)
 
 
 
