2647e831f2023bcaf157335f3350c98a591e8f11
Andre Sousa <up202005277@up.pt>
2022-10-07 19:02:42 +0100

    I dont know whats going on

--- project.hs
+++ project.hs
@@ -2,27 +2,46 @@ import Data.Char
 import Data.List
 import Data.Maybe (fromJust, isJust)
 
-data Termo = Termo Coef Variable Exp deriving (Eq, Show)
+type Polynom = [Termo]
 
--- type Sign = Char
+data Termo = Termo { coef :: Int, variable :: String, exp :: Int } deriving (Eq, Show)
 
-type Coef = Int
+(!!) :: (Int, String, Int) -> Int -> Either Int String
+(!!) (a:b:c:_) idx
+            | idx == 0 = a
+            | idx == 1 = b
+            | idx == 2 = c
+            | otherwise = error "OUT OF BOUNDS"
 
-type Variable = String
+sumToIndex :: Int -> Int -> Int -> Polynom -> Polynom
+sumToIndex element idx currentIdx (p:ps)
+                          | idx == currentIdx = Termo ((extractValues p Main.!! 0) + element) ((extractValues p) Main.!! 1) ((extractValues p) Main.!! 2) : ps
+                          | otherwise = p : sumToIndex element idx (currentIdx+1) ps
 
-type Exp = Int
+
+extractValues :: Termo -> (Int, String, Int)
+extractValues (Termo a b c) = (a, b, c)
+
+getVariableList :: Polynom -> [String]
+getVariableList p = [extractValues tmp Main.!! 1 | tmp <- p]
 
 convertToExp :: String -> Int
 convertToExp [] = 1
-convertToExp (x:xs)
+convertToExp (x : xs)
   | x == '+' && not (null xs) = read xs :: Int
-  | x == '-' && not (null xs) = read (x:xs) :: Int
+  | x == '-' && not (null xs) = read (x : xs) :: Int
   | x == '-' && null xs = -1
-  | all isDigit (x:xs) = read (x:xs) :: Int
+  | all isDigit (x : xs) = read (x : xs) :: Int
   | otherwise = 1
 
-wordSplit :: String -> [Termo]
-wordSplit str = [Termo (convertToExp (signal : takeWhile isDigit uterm)) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | idx_term <- [0..length (words str)-1], let uterm = words str !! idx_term; idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0; signal = if idx_term > 0 && head (words str !! (idx_term-1)) == '-' then '-' else '+', uterm /= "+" && uterm /= "-"]
+wordSplit :: String -> Polynom
+wordSplit str = [Termo (convertToExp (signal : takeWhile isDigit uterm)) (takeWhile isAlpha (dropWhile (\chr -> isDigit chr || chr == '*') uterm)) (convertToExp (dropWhile (const False) (drop idx uterm))) | idx_term <- [0 .. length (words str) -1], let uterm = words str Prelude.!! idx_term; idx = if Data.Maybe.isJust (elemIndex '^' uterm) then fromJust (elemIndex '^' uterm) + 1 else 0; signal = if idx_term > 0 && head (words str Prelude.!! (idx_term -1)) == '-' then '-' else '+', uterm /= "+" && uterm /= "-"]
+
+addTerms :: Polynom -> Polynom
+addTerms (p1:ps)
+            | Data.Maybe.isJust(elemIndex (head lista) (tail lista)) = addTerms sumToIndex(extractValues p1 Main.!! 0, fromJust(elemIndex (head lista) (tail lista)), 0, ps)
+
+
 
 {-
 stringToInternal :: [Termo] -> IO ()
